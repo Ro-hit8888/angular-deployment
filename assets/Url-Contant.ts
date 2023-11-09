a9a7926776a0b2037bd758cfe09a841592ba2a0a
Ro-hit8888 <rohithgoudp30@gmail.com>
2023-11-09 16:36:32 +0530

    2nd commiting

--- assets/Url-Contant.ts
+++ assets/Url-Contant.ts
@@ -1,75 +1,75 @@
 export class UrlContant {
-  public static readonly authenticateUrl = 'http://54.161.33.80:8080/authenticate';
+  public static readonly authenticateUrl = 'http://35.175.213.222:8080/authenticate';
 
 
-  public static readonly CreateRoleUrl = 'http://54.161.33.80:8080/role/createRole';
+  public static readonly CreateRoleUrl = 'http://35.175.213.222:8080/role/createRole';
 
-  public static readonly EditRoleUrl = 'http://54.161.33.80:8080/role/editeRole';
+  public static readonly EditRoleUrl = 'http://35.175.213.222:8080/role/editeRole';
 
-  public static readonly SearchRoleUrl = 'http://54.161.33.80:8080/role/searchRoleUrl';
+  public static readonly SearchRoleUrl = 'http://35.175.213.222:8080/role/searchRoleUrl';
 
-  public static readonly GetRoleByIDUrl = 'http://54.161.33.80:8080/role/getRoleByID';
+  public static readonly GetRoleByIDUrl = 'http://35.175.213.222:8080/role/getRoleByID';
 
-  public static readonly DeleteRoleByIDUrl = 'http://54.161.33.80:8080/role/deleteRoleByID';
-
-
-  public static readonly CreateRewardUrl = 'http://54.161.33.80:8080/reward/createReward';
-
-  public static readonly EditRewardUrl = 'http://54.161.33.80:8080/reward/editReward';
-
-  public static readonly SearchRewardUrl = 'http://54.161.33.80:8080/reward/getAllReward';
-
-  public static readonly GetRewardByIDUrl = 'http://54.161.33.80:8080/reward/getRewardByID';
-
-  public static readonly DeleteRewardByIDUrl = 'http://54.161.33.80:8080/reward/deleteRewardByID';
-
-  public static readonly GetLeaderBoardDetailsUrl = 'http://54.161.33.80:8080/reward/getLeaderBoardDetails';
+  public static readonly DeleteRoleByIDUrl = 'http://35.175.213.222:8080/role/deleteRoleByID';
 
 
-  public static readonly LoginUrl = 'http://54.161.33.80:8080/user/login';
+  public static readonly CreateRewardUrl = 'http://35.175.213.222:8080/reward/createReward';
+
+  public static readonly EditRewardUrl = 'http://35.175.213.222:8080/reward/editReward';
+
+  public static readonly SearchRewardUrl = 'http://35.175.213.222:8080/reward/getAllReward';
+
+  public static readonly GetRewardByIDUrl = 'http://35.175.213.222:8080/reward/getRewardByID';
+
+  public static readonly DeleteRewardByIDUrl = 'http://35.175.213.222:8080/reward/deleteRewardByID';
+
+  public static readonly GetLeaderBoardDetailsUrl = 'http://35.175.213.222:8080/reward/getLeaderBoardDetails';
+
+
+  public static readonly LoginUrl = 'http://35.175.213.222:8080/user/login';
   
-  public static readonly CreateUserUrl = 'http://54.161.33.80:8080/user/createUser';
+  public static readonly CreateUserUrl = 'http://35.175.213.222:8080/user/createUser';
 
-  public static readonly EditUserUrl = 'http://54.161.33.80:8080/user/editUser';
+  public static readonly EditUserUrl = 'http://35.175.213.222:8080/user/editUser';
 
-  public static readonly SearchUserUrl = 'http://54.161.33.80:8080/user/getAllUser';
+  public static readonly SearchUserUrl = 'http://35.175.213.222:8080/user/getAllUser';
 
-  public static readonly GetUserByIDUrl = 'http://54.161.33.80:8080/user/getUserByID';
+  public static readonly GetUserByIDUrl = 'http://35.175.213.222:8080/user/getUserByID';
 
-  public static readonly DeleteUserByIDUrl = 'http://54.161.33.80:8080/user/deleteUserByID';
+  public static readonly DeleteUserByIDUrl = 'http://35.175.213.222:8080/user/deleteUserByID';
 
-  public static readonly AssignUserEventUrl = 'http://54.161.33.80:8080/user/assignUserEvent';
+  public static readonly AssignUserEventUrl = 'http://35.175.213.222:8080/user/assignUserEvent';
 
-  public static readonly processUserFromEventUrl = 'http://54.161.33.80:8080/user/processUserFromEvent';
+  public static readonly processUserFromEventUrl = 'http://35.175.213.222:8080/user/processUserFromEvent';
 
-  public static readonly getAllUserFromEventUrl = 'http://54.161.33.80:8080/user/getAllUserFromEvent';
+  public static readonly getAllUserFromEventUrl = 'http://35.175.213.222:8080/user/getAllUserFromEvent';
 
-  public static readonly deleteUserFromEventUrl = 'http://54.161.33.80:8080/user/deleteUserFromEvent';
+  public static readonly deleteUserFromEventUrl = 'http://35.175.213.222:8080/user/deleteUserFromEvent';
 
-  public static readonly getAllUserEventUrl = 'http://54.161.33.80:8080/user/getAllUserEvent';
+  public static readonly getAllUserEventUrl = 'http://35.175.213.222:8080/user/getAllUserEvent';
 
-  public static readonly getAllUserEventsByIDUrl = 'http://54.161.33.80:8080/user/getAllUserEventsByID';
-
-
-  public static readonly CreateContactUrl = 'http://54.161.33.80:8080/contact/createContact';
-
-  public static readonly EditContactUrl = 'http://54.161.33.80:8080/contact/editContact';
-
-  public static readonly SearchContactUrl = 'http://54.161.33.80:8080/contact/getAllContact';
-
-  public static readonly GetContactByIDUrl = 'http://54.161.33.80:8080/contact/getContactByID';
-
-  public static readonly DeleteContactByIDUrl = 'http://54.161.33.80:8080/contact/deleteContactByID';
+  public static readonly getAllUserEventsByIDUrl = 'http://35.175.213.222:8080/user/getAllUserEventsByID';
 
 
-  public static readonly CreateEventUrl = 'http://54.161.33.80:8080/event/createEvent';
+  public static readonly CreateContactUrl = 'http://35.175.213.222:8080/contact/createContact';
 
-  public static readonly EditEventUrl = 'http://54.161.33.80:8080/event/editEvent';
+  public static readonly EditContactUrl = 'http://35.175.213.222:8080/contact/editContact';
 
-  public static readonly SearchEventUrl = 'http://54.161.33.80:8080/event/getAllEvent';
+  public static readonly SearchContactUrl = 'http://35.175.213.222:8080/contact/getAllContact';
 
-  public static readonly GetEventByIDUrl = 'http://54.161.33.80:8080/event/getEventByID';
+  public static readonly GetContactByIDUrl = 'http://35.175.213.222:8080/contact/getContactByID';
 
-  public static readonly DeleteEventByIDUrl = 'http://54.161.33.80:8080/event/deleteEventByID';
+  public static readonly DeleteContactByIDUrl = 'http://35.175.213.222:8080/contact/deleteContactByID';
+
+
+  public static readonly CreateEventUrl = 'http://35.175.213.222:8080/event/createEvent';
+
+  public static readonly EditEventUrl = 'http://35.175.213.222:8080/event/editEvent';
+
+  public static readonly SearchEventUrl = 'http://35.175.213.222:8080/event/getAllEvent';
+
+  public static readonly GetEventByIDUrl = 'http://35.175.213.222:8080/event/getEventByID';
+
+  public static readonly DeleteEventByIDUrl = 'http://35.175.213.222:8080/event/deleteEventByID';
 
 }
